b08987cb8995dd734031190dda79440ed0662367
Alexandr Ragulin <klodiys62@gmail.com>
2023-06-22 23:09:19 +0300

    make all datatypes public

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -4,10 +4,10 @@ use chrono::NaiveTime;
 use std::collections::HashMap;
 use std::sync::Arc;
 
-type SharedString = Arc<str>;
-type SharedInt = Arc<u32>;
-type SharedPeriodicDateTime = Arc<Timestamp>;
-type SharedSlice<T> = Arc<[T]>;
+pub type SharedString = Arc<str>;
+pub type SharedInt = Arc<u32>;
+pub type SharedPeriodicDateTime = Arc<Timestamp>;
+pub type SharedSlice<T> = Arc<[T]>;
 
 /// A word is a non-empty sequence of non-whitespace characters.
 /// Here are a few test cases:
@@ -62,32 +62,32 @@ impl SharedLine {
 }
 
 #[derive(Debug)]
-enum DelayType {
+pub enum DelayType {
     Minus,      // affects all occurrences
     MinusMinus, // affects only the first scheduled occurrence
 }
 
 #[derive(Debug)]
-enum RepeaterType {
+pub enum RepeaterType {
     Plus,     // add period to time, displayed as "+"
     PlusPlus, // keep adding period to time until time reaches future, displayed as "++"
     DotPlus,  // add period to current time, displayed as ".+"
 }
 
 #[derive(Debug)]
-struct Repeater {
+pub struct Repeater {
     repeat: Duration,
     periodicity_type: RepeaterType,
 }
 
 #[derive(Debug)]
-struct Delay {
+pub struct Delay {
     delay: Duration,
     delay_type: DelayType,
 }
 
 #[derive(Debug)]
-struct Timestamp {
+pub struct Timestamp {
     date: NaiveDate,
     start_time: Option<NaiveTime>,
     end_time: Option<NaiveTime>,
