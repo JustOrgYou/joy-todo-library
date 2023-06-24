918f595ecf7ad7e4e66d27de4fe291e49b5dad97
Alexandr Ragulin <klodiys62@gmail.com>
2023-06-24 11:02:29 +0300

    simplify initial implementation

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -2,64 +2,6 @@ use chrono::Duration;
 use chrono::NaiveDate;
 use chrono::NaiveTime;
 use std::collections::HashMap;
-use std::sync::Arc;
-
-pub type SharedString = Arc<str>;
-pub type SharedInt = Arc<u32>;
-pub type SharedPeriodicDateTime = Arc<Timestamp>;
-pub type SharedSlice<T> = Arc<[T]>;
-
-/// A word is a non-empty sequence of non-whitespace characters.
-/// Here are a few test cases:
-/// ```
-/// use joy_todo_library::datatypes::SharedWord;
-///
-/// assert!(SharedWord::new("word".into()).is_some());
-/// assert!(SharedWord::new("also_word".into()).is_some());
-/// assert!(SharedWord::new("not word".into()).is_none());
-/// assert!(SharedWord::new("".into()).is_none());
-/// ```
-#[derive(Debug, PartialEq)]
-pub struct SharedWord(SharedString);
-
-impl SharedWord {
-    pub fn new(word: SharedString) -> Option<Self> {
-        if word.len() == 0 || word.chars().any(|c| c.is_whitespace()) {
-            None
-        } else {
-            Some(SharedWord(word))
-        }
-    }
-
-    pub fn as_str(&self) -> &str {
-        &self.0
-    }
-}
-
-/// A line is a sequence of characters that does not contain a newline.
-/// Here are a few test cases:
-/// ```
-/// use joy_todo_library::datatypes::SharedLine;
-///
-///assert!(SharedLine::new("".into()).is_some());
-///assert!(SharedLine::new("line".into()).is_some());
-///assert!(SharedLine::new("line\nline".into()).is_none());
-/// ```
-#[derive(Debug, PartialEq)]
-pub struct SharedLine(SharedString);
-
-impl SharedLine {
-    pub fn new(line: SharedString) -> Option<Self> {
-        if line.chars().any(|c| c == '\n') {
-            None
-        } else {
-            Some(SharedLine(line))
-        }
-    }
-    pub fn as_str(&self) -> &str {
-        &self.0
-    }
-}
 
 #[derive(Debug)]
 pub enum DelayType {
@@ -97,14 +39,14 @@ pub struct Timestamp {
 
 #[derive(Debug)]
 pub struct Task {
-    title: SharedLine,
-    body: SharedString,
-    children: SharedSlice<Task>,
-    priority: Option<SharedInt>,
-    keyword: SharedWord,
-    tags: SharedSlice<SharedWord>,
-    scheduled: Option<SharedPeriodicDateTime>,
-    deadline: Option<SharedPeriodicDateTime>,
-    created: Option<SharedPeriodicDateTime>,
-    properties: HashMap<SharedWord, SharedString>,
+    title: String,
+    body: String,
+    children: Vec<Task>,
+    priority: Option<u32>,
+    keyword: String,
+    tags: Vec<String>,
+    scheduled: Option<Timestamp>,
+    deadline: Option<Timestamp>,
+    created: Option<Timestamp>,
+    properties: HashMap<String, String>,
 }
